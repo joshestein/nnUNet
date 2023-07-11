c21c89f8057eb57d7ee58bfe6e7a30a03768b77a
Josh <josh.e.stein@gmail.com>
2023-07-11 23:28:46 +0200

    Predict and evaluate all runs

--- nnunetv2/evaluation/evaluate_all.py
+++ nnunetv2/evaluation/evaluate_all.py
@@ -1,30 +1,69 @@
 import os
-import subprocess
 
+from nnunetv2.inference.predict_all import run_subprocess
 from nnunetv2.paths import nnUNet_raw, nnUNet_results
 from nnunetv2.utilities.dataset_name_id_conversion import maybe_convert_to_dataset_name
 
-for dataset_id in [27]:
-    dataset_name = maybe_convert_to_dataset_name(dataset_id)
-    for config in ["2d"]:
-        for num_cases in [8, 12, 16, 24, 32, 48, 64, 80, 96, 144, 160, 192, 240]:
-            gt_folder = os.path.join(nnUNet_raw, dataset_name, "labelsTs")
-            pred_folder = os.path.join(
-                nnUNet_results, dataset_name, f"imagesTs_pred_{config}", f"num_training_cases_{num_cases:03d}"
-            )
 
-            dataset_file = os.path.join(pred_folder, "dataset.json")
-            plans_file = os.path.join(pred_folder, "plans.json")
-            args = [gt_folder, pred_folder, "-pfile", plans_file, "-djfile", dataset_file]
-            try:
-                p = subprocess.Popen(f"python evaluate_predictions.py {' '.join(args)}", shell=True)
-                out, err = p.communicate()
-                print(out)
-            except:
-                print(f"Failed for pred folder: {pred_folder}. Does the run/fold exist?")
-                continue
+def build_args(pred_folder: str, dataset_name: str):
+    gt_folder = os.path.join(nnUNet_raw, dataset_name, "labelsTs")
+    dataset_file = os.path.join(pred_folder, "dataset.json")
+    plans_file = os.path.join(pred_folder, "plans.json")
+    return [gt_folder, pred_folder, "-pfile", plans_file, "-djfile", dataset_file]
 
-# TODO:
-# folder_name = "None"
-# args = ["-d", dataset_id, "-f", 0, "-c", config, "-i", "imagesTs", "-o", f"imagesTs_pred_192"]
-# predict_entry_point(args)
+
+def predict_num_training_cases(dataset_name: str, config: str):
+    for num_cases in [8, 12, 16, 24, 32, 48, 64, 80, 96, 144, 160, 192, 240]:
+        pred_folder = os.path.join(
+            nnUNet_results, dataset_name, f"imagesTs_pred_{config}", f"num_training_cases_{num_cases:03d}"
+        )
+        try:
+            run_subprocess("evaluate_predictions.py", build_args(pred_folder, dataset_name))
+        except FileNotFoundError:
+            print(f"Failed for pred folder: {pred_folder}. Does the run/fold exist?")
+            continue
+
+
+def predict_slice_regions(dataset_name: str, config: str):
+    for slice_regions in [
+        ("apex", "mid", "base"),
+        ("apex", "mid"),
+        ("apex", "base"),
+        ("mid", "base"),
+        ["apex"],
+        ["mid"],
+        ["base"],
+    ]:
+        pred_folder = os.path.join(
+            nnUNet_results, dataset_name, f"imagesTs_pred_{config}", f"slice_regions_{'_'.join(slice_regions)}"
+        )
+        try:
+            run_subprocess("evaluate_predictions.py", build_args(pred_folder, dataset_name))
+        except FileNotFoundError:
+            print(f"Failed for pred folder: {pred_folder}. Does the run/fold exist?")
+            continue
+
+
+def predict_percentage_slices(dataset_name: str, config: str):
+    for percentage_slices in [1.0, 0.8, 0.66, 0.5, 0.33, 0.2, 0.1, 0.05]:
+        pred_folder = os.path.join(
+            nnUNet_results, dataset_name, f"imagesTs_pred_{config}", f"percentage_slices_{percentage_slices}"
+        )
+        try:
+            run_subprocess("evaluate_predictions.py", build_args(pred_folder, dataset_name))
+        except FileNotFoundError:
+            print(f"Failed for pred folder: {pred_folder}. Does the run/fold exist?")
+            continue
+
+
+def main():
+    for dataset_id in [27, 114]:
+        dataset_name = maybe_convert_to_dataset_name(dataset_id)
+        for config in ["2d", "3d_fullres"]:
+            predict_num_training_cases(dataset_name, config)
+            predict_slice_regions(dataset_name, config)
+            predict_percentage_slices(dataset_name, config)
+
+
+if __name__ == "__main__":
+    main()
--- nnunetv2/inference/predict_all.py
+++ nnunetv2/inference/predict_all.py
@@ -2,43 +2,97 @@ import os
 
 import subprocess
 
-for dataset_id in [27]:
-    for config in ["2d"]:
-        for slice_regions in [
-            ("apex", "mid", "base"),
-            ("apex", "mid"),
-            ("apex", "base"),
-            ("mid", "base"),
-            ["apex"],
-            ["mid"],
-            ["base"],
-        ]:
-            output_folder = os.path.join(f"imagesTs_pred_{config}", f"slice_regions_{'_'.join(slice_regions)}")
-            args = [
-                "-d",
-                str(dataset_id),
-                "-f",
-                str(0),
-                "-c",
-                config,
+
+def run_subprocess(script: str, args: list[str]):
+    assert script.endswith(".py")
+    p = subprocess.Popen(f"python {script} {' '.join(args)}", shell=True)
+    out, err = p.communicate()
+    print(out)
+
+
+def build_base_args(dataset_id: int, config: str):
+    return [
+        "-d",
+        str(dataset_id),
+        "-f",
+        str(0),
+        "-c",
+        config,
+        "-i",
+        "imagesTs",
+    ]
+
+
+def evaluate_num_training_cases(args: list[str]):
+    for num_cases in [8, 12, 16, 24, 32, 48, 64, 80, 96, 144, 160, 192, 240]:
+        output_folder = os.path.join(f"imagesTs_pred_{config}", f"num_training_cases_{num_cases:03d}")
+        args.extend(
+            [
                 "-m",
-                f"num_training_cases_None/percentage_slices_1.0_regions_{'_'.join(slice_regions)}",
-                "-i",
-                "imagesTs",
+                f"num_training_patients_{num_cases}",
                 "-o",
                 output_folder,
-                "-device",
-                "cpu",
             ]
-            try:
-                p = subprocess.Popen(f"python predict_from_raw_data.py {' '.join(args)}", shell=True)
-                out, err = p.communicate()
-                print(out)
-            except:
-                print(f"Failed for regions {slice_regions}. Does the run/fold exist?")
-                continue
+        )
+        try:
+            run_subprocess("predict_from_raw_data.py", args)
+        except FileNotFoundError:
+            print(f"Failed for num_training_cases_{num_cases}. Does the run/fold exist?")
+            continue
 
-# TODO:
-# folder_name = "None"
-# args = ["-d", dataset_id, "-f", 0, "-c", config, "-i", "imagesTs", "-o", f"imagesTs_pred_192"]
-# predict_entry_point(args)
+
+def evaluate_slice_regions(args: list[str]):
+    for slice_regions in [
+        ("apex", "mid", "base"),
+        ("apex", "mid"),
+        ("apex", "base"),
+        ("mid", "base"),
+        ["apex"],
+        ["mid"],
+        ["base"],
+    ]:
+        output_folder = os.path.join(f"imagesTs_pred_{config}", f"slice_regions_{'_'.join(slice_regions)}")
+        args.extend(
+            [
+                "-m",
+                f"num_training_cases_None/percentage_slices_1.0_regions_{'_'.join(slice_regions)}",
+                "-o",
+                output_folder,
+            ]
+        )
+        try:
+            run_subprocess("predict_from_raw_data.py", args)
+        except FileNotFoundError:
+            print(f"Failed for regions {slice_regions}. Does the run/fold exist?")
+            continue
+
+
+def evaluate_percentage_slices(args: list[str]):
+    for percentage_slices in [1.0, 0.8, 0.66, 0.5, 0.33, 0.2, 0.1, 0.05]:
+        output_folder = os.path.join(f"imagesTs_pred_{config}", f"percentage_slices_{percentage_slices}")
+        args.extend(
+            [
+                "-m",
+                f"percentage_slices_{percentage_slices}",
+                "-o",
+                output_folder,
+            ]
+        )
+        try:
+            run_subprocess("predict_from_raw_data.py", args)
+        except FileNotFoundError:
+            print(f"Failed for percentage slices {percentage_slices}. Does the run/fold exist?")
+            continue
+
+
+def main():
+    for dataset_id in [27, 114]:
+        for config in ["2d", "3d_fullres"]:
+            base_args = build_base_args(dataset_id, config)
+            evaluate_num_training_cases(base_args)
+            evaluate_slice_regions(base_args)
+            evaluate_percentage_slices(base_args)
+
+
+if __name__ == "__main__":
+    main()
